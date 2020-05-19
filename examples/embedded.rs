e5212167ebd1c9d3acec7993b6f54412227930ef
Tglman <tglman@tglman.com>
2020-05-20 00:07:30 +0100

    add examples for embedded, changed logic of basic test

--- examples/embedded.rs
+++ examples/embedded.rs
@@ -1,17 +1,31 @@
 use structsy::{Structsy, StructsyError, StructsyTx};
-use structsy_derive::Persistent;
+use structsy_derive::{Persistent, PersistentEmbedded};
 
 #[derive(Persistent, Debug, PartialEq)]
 struct MyData {
     #[index(mode = "cluster")]
     name: String,
-    address: String,
+    address: Address,
 }
 impl MyData {
-    fn new(name: &str, address: &str) -> MyData {
+    fn new(name: &str, address: Address) -> MyData {
         MyData {
             name: name.to_string(),
-            address: address.to_string(),
+            address,
+        }
+    }
+}
+
+#[derive(PersistentEmbedded, Debug, PartialEq)]
+struct Address {
+    host: String,
+    port: u16,
+}
+impl Address {
+    fn new(host: &str, port: u16) -> Address {
+        Address {
+            host: host.to_string(),
+            port,
         }
     }
 }
@@ -20,14 +34,14 @@ fn main() -> Result<(), StructsyError> {
     let db = Structsy::open("my_data.db")?;
     db.define::<MyData>()?;
 
-    let my_data = MyData::new("Structsy", "https://gitlab.com/tglman/structsy");
+    let my_data = MyData::new("My host", Address::new("127.0.0.1", 2424));
     let mut tx = db.begin()?;
     let id = tx.insert(&my_data)?;
     tx.commit()?;
 
     let mut iter = db.scan::<MyData>()?;
     let (_id, data) = iter.next().unwrap();
-    assert_eq!(data.name, "Structsy".to_string());
+    assert_eq!(data.name, "My Host");
 
     let mut tx = db.begin()?;
     tx.delete(&id)?;
