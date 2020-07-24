93ef7250122b0b9f209b1e35b3b9dc39f18b0974
Tglman <tglman@tglman.com>
2020-07-24 21:59:16 +0100

    add example of structsy inside actix-web

---
+++ actix_web/src/main.rs
@@ -0,0 +1,136 @@
+use actix_web::{web, App, HttpResponse, HttpServer, ResponseError};
+use serde::{Deserialize, Serialize};
+use std::io::Error as IOError;
+use structsy::{Structsy, StructsyError, StructsyTx};
+use structsy_derive::Persistent;
+
+#[derive(Debug)]
+enum Error {
+    StructsyError(StructsyError),
+    ActixError(actix_web::Error),
+    IOError(IOError),
+}
+
+impl From<StructsyError> for Error {
+    fn from(e: StructsyError) -> Self {
+        Error::StructsyError(e)
+    }
+}
+impl From<actix_web::Error> for Error {
+    fn from(e: actix_web::Error) -> Self {
+        Error::ActixError(e)
+    }
+}
+impl From<IOError> for Error {
+    fn from(e: IOError) -> Self {
+        Error::IOError(e)
+    }
+}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+impl ResponseError for Error {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::InternalServerError().body(&format!("{:?}", &self))
+    }
+}
+
+#[derive(Serialize, Deserialize, Persistent)]
+struct Coffee {
+    brand: String,
+    size: u32,
+    time: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CoffeeList {
+    coffees: Vec<(String, Coffee)>,
+}
+
+#[derive(Serialize, Deserialize, Persistent)]
+struct Beer {
+    brand: String,
+    size: u32,
+    time: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct BeerList {
+    beers: Vec<(String, Beer)>,
+}
+
+async fn drink_coffee(
+    (coffee, db): (web::Json<Coffee>, web::Data<Structsy>),
+) -> Result<HttpResponse, Error> {
+    let mut tx = db.begin()?;
+    tx.insert(&coffee.0)?;
+    tx.commit()?;
+    Ok(HttpResponse::from("OK"))
+}
+async fn list_coffees(db: web::Data<Structsy>) -> Result<HttpResponse, Error> {
+    let mut coffees = Vec::new();
+    for (id, coffee) in db.scan::<Coffee>()? {
+        coffees.push((id.to_string(), coffee));
+    }
+    Ok(HttpResponse::Ok().json(CoffeeList { coffees }))
+}
+async fn delete_coffee((db, id): (web::Data<Structsy>, String)) -> Result<HttpResponse, Error> {
+    let p_id: structsy::Ref<Coffee> = id.parse()?;
+    let mut tx = db.begin()?;
+    tx.delete(&p_id)?;
+    tx.commit()?;
+    Ok(HttpResponse::from("OK"))
+}
+
+async fn drink_beer(
+    (beer, db): (web::Json<Beer>, web::Data<Structsy>),
+) -> Result<HttpResponse, Error> {
+    let mut tx = db.begin()?;
+    tx.insert(&beer.0)?;
+    tx.commit()?;
+    Ok(HttpResponse::from("OK"))
+}
+async fn list_beers(db: web::Data<Structsy>) -> Result<HttpResponse, Error> {
+    let mut beers = Vec::new();
+    for (id, beer) in db.scan::<Beer>()? {
+        beers.push((id.to_string(), beer));
+    }
+    Ok(HttpResponse::Ok().json(BeerList { beers }))
+}
+async fn delete_beer((db, id): (web::Data<Structsy>, String)) -> Result<HttpResponse, Error> {
+    let p_id: structsy::Ref<Beer> = id.parse()?;
+    let mut tx = db.begin()?;
+    tx.delete(&p_id)?;
+    tx.commit()?;
+    Ok(HttpResponse::from("OK"))
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), Error> {
+    let structsy = Structsy::open("track.db")?;
+    structsy.define::<Coffee>()?;
+    structsy.define::<Beer>()?;
+    let data_persistence = web::Data::new(structsy);
+    HttpServer::new(move || {
+        App::new()
+            .data(data_persistence.clone())
+            .service(
+                web::scope("coffee")
+                    .service(web::resource("create").route(web::post().to(drink_coffee)))
+                    .service(web::resource("list").route(web::get().to(list_coffees)))
+                    .service(web::resource("delete").route(web::get().to(delete_coffee))),
+            )
+            .service(
+                web::scope("beer")
+                    .service(web::resource("create").route(web::post().to(drink_beer)))
+                    .service(web::resource("list").route(web::get().to(list_beers)))
+                    .service(web::resource("delete").route(web::get().to(delete_beer))),
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await?;
+    Ok(())
+}
